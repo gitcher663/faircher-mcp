934b6cbf9a5dc0e91853b4ddd56591b227e85af7
gitcher663 <nick@faircher.com>
2026-01-13 17:10:09 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = Number(process.env.PORT) || 8080;
 
 const manifest = {
   name: "FairCher MCP",
