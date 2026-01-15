7154a372c5354078fa772e8922d661b6c5833fd9
gitcher663 <nick@faircher.com>
2026-01-15 15:16:40 -0500

    Update index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,7 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { checkAdvertisingActivity } from "./tools/checkAdvertisingActivity.js";
 
 export const server = new McpServer({
-  name: "FairCher",
+  name: "faircher",
   version: "1.0.0",
 });
 
